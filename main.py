cebe5b46a26b757c7479d5de33bdc74ed2d2fc49
doggo <komunistdoggo420@gmail.com>
2021-03-30 13:37:54 -0500

    Changed the muted command, added comments, added local json file dependency.

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ bot_client = commands.Bot(command_prefix='?')
 
 @bot_client.command()
 @commands.has_permissions(administrator=True)
-async def mute(ctx, member: discord.Member, reason="No reason given"):
-    muted_role = discord.utils.get(ctx.guild.roles, name="Muted")
+async def mute(ctx, member: discord.Member, reason="No reason given", muted_role_name="Muted"):
+    muted_role = discord.utils.get(ctx.guild.roles, name=muted_role_name)
 
+    ## Creates muted role if it doesn't exist
     if not muted_role:
         muted_role = await ctx.guild.create_role(name="Muted")
-
         for channel in ctx.guild.channels:
             await channel.set_permissions(muted_role, speak=False, send_messages=False, add_reactions=False)
 
@@ -28,6 +28,11 @@ async def mute(ctx, member: discord.Member, reason="No reason given"):
 @commands.has_permissions(administrator=True)
 async def unmute(ctx, member: discord.Member, muted_role_name="Muted"):
     muted_role = discord.utils.get(ctx.guild.roles, name=muted_role_name)
+
+    ## Checks if overloaded muted role inputted wrong
+    if not muted_role:
+        await ctx.send(f"Muted role {muted_role_name} does not exist.")
+        return
 
     await member.remove_roles(muted_role)
     await member.send(f"You have been unmuted in {ctx.guild.name}.")
@@ -42,13 +47,21 @@ async def kick(ctx, member: discord.Member, reason="No reason given"):
     await ctx.send(f"{member.name} has been kicked for {reason}.")
 
 
-
 @bot_client.command()
 async def get_role(ctx, name):
-    role = discord.utils.get(ctx.guild.roles, name=name)
+    role = discord.utils.get(ctx.guild.roles, name=name.title())
 
+    ## Checks if role exists
     if not role:
         await ctx.send(f"{name} is not a role.")
+        return
+
+    ## Checks if role is too high in role hierarchy
+    try:
+        with open("Info/ServerInfo.json", "r") as json_file:
+            ctx.guild.roles.index(role, ctx.guild.roles.index(discord.utils.get(ctx.guild.roles, name=json.dump(json_file)["highest_get_role"])))
+    except ValueError:
+        await ctx.send(f"Unable to get role {role.name}, too high in role hierarchy.")
         return
 
     await ctx.author.add_roles(role)
@@ -57,8 +70,9 @@ async def get_role(ctx, name):
 
 @bot_client.command()
 async def remove_role(ctx, name):
-    role = discord.utils.get(ctx.guild.roles, name=name)
+    role = discord.utils.get(ctx.guild.roles, name=name.title())
 
+    # Checks if role exists
     if not role:
         await ctx.send(f"{name} is not a role.")
         return
@@ -68,12 +82,12 @@ async def remove_role(ctx, name):
     await ctx.send(f"The role {role.name} has been removed from {ctx.author.name}.")
 
 
-
 @bot_client.command()
 @commands.has_permissions(administrator=True)
 async def give_role(ctx, member: discord.Member, name):
-    role = discord.utils.get(ctx.guild.roles, name=name)
+    role = discord.utils.get(ctx.guild.roles, name=name.title())
 
+    ## Checks if role exists
     if not role:
         await ctx.send(f"{name} is not a role.")
         return
@@ -86,8 +100,9 @@ async def give_role(ctx, member: discord.Member, name):
 @bot_client.command()
 @commands.has_permissions(administrator=True)
 async def take_role(ctx, member: discord.Member, name):
-    role = discord.utils.get(ctx.guild.roles, name=name)
+    role = discord.utils.get(ctx.guild.roles, name=name.title())
 
+    ## Checks if role exists
     if not role:
         await ctx.send(f"{name} is not a role.")
         return
